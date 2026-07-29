7166e2fea4075d1c0c0050a90ee1d84cf7f84f02
quagmire49 <geeteshkovel@gmail.com>
2026-07-29 20:17:37 +0530

    feat: implement note creation, detail view, and not found component with API integration

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,7 +1,7 @@
 import { PenSquareIcon, Trash2Icon } from "lucide-react";
 import { Link } from "react-router";
 import { formatDate } from "../lib/utils";
-// import api from "../lib/axios";
+import api from "../lib/axios";
 import toast from "react-hot-toast";
 
 const NoteCard = ({ note, setNotes }) => {
