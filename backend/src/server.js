ad54a5599d1f37b934228339a7245f10b0f328a0
quagmire49 <geeteshkovel@gmail.com>
2026-07-29 19:56:25 +0530

    feat: implement note management features with rate limiting and UI enhancements

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,20 @@
 import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
 import router from "./routes/notesRoutes.js";
 import connectDB from "./config/db.js";
-import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js";
+
 
 dotenv.config();
 
 const app=express();
 const PORT=process.env.PORT || 8000
 
+app.use(cors({
+  orgin:"http://localhost:5173",
+}))
 app.use(express.json())
 app.use(rateLimiter)
 app.use("/api/notes",router);
